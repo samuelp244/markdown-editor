d8b5b5aa58a9fbb6b0b979dec9e559b291b98f42
samuelp244 <samuelp244@gmail.com>
2024-05-15 11:01:28 +0530

    adds ui improvements

--- client/src/components/textAreas/MdEditor.tsx
+++ client/src/components/textAreas/MdEditor.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Textarea } from "@nextui-org/input";
 import { Socket, io } from "socket.io-client";
 import { DefaultEventsMap } from "@socket.io/component-emitter";
 
@@ -11,40 +10,39 @@ let socket: Socket<DefaultEventsMap, DefaultEventsMap>;
 const MdEditor = () => {
   const [markdown, setMarkdown] = useState("");
   const [htmlRes, setHtmlRes] = useState("");
+  
   useEffect(() => {
     socket = io(BACKEND_URL);
   }, []);
+
   useEffect(() => {
     socket.on("htmlRes", ({ html }) => {
       setHtmlRes(html);
     });
   });
-  const handleMarkdownInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+  const handleMarkdownInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const { value } = e.target;
     setMarkdown(value);
     // Emit Markdown text to the backend
     socket.emit("markdown", { markdownText: value });
   };
+  
   return (
-    <>
-      <div>
-        <Textarea
-          label='Mark down'
-          placeholder='Enter your Mark down Code'
-          className=''
+    <div className='col-span-1 sm:col-span-2 pt-20 h-full overflow-hidden'>
+      <div className='h-full grid grid-cols-1 sm:grid-cols-2 w-full'>
+        <textarea
+          placeholder='Enter your Markdown Code'
+          className='bg-gray-800 text-white p-4 resize-none  max-sm:min-h-[45vh]'
           value={markdown}
           onChange={handleMarkdownInput}
-          color='secondary'
-          minRows={12}
+        />
+        <div
+          className='bg-white text-black p-4 overflow-y-auto'
+          dangerouslySetInnerHTML={{ __html: htmlRes }}
         />
       </div>
-
-      {/* <p>Here is you rendered html</p> */}
-      <div
-        className='flex flex-col gap-3 bg-white h-full rounded-xl text-black'
-        dangerouslySetInnerHTML={{ __html: htmlRes }}
-      />
-    </>
+    </div>
   );
 };
 
